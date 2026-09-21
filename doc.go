dd73a1bdcd6aa8155449ede8931828fa027fcd9f
em-ily-dev <160207232+em-ily-dev@users.noreply.github.com>
2026-09-21 12:12:56 -0700

    domi: simplify js bundling instructions (#185)
    Co-authored-by: emily <em@cute.invalid>

--- doc.go
+++ doc.go
@@ -96,7 +96,7 @@
 // to add domi.js into their JavaScript bundle.
 // Obtain the filesystem path of domi.js by running:
 //
-//	go list -m -f '{{.Dir}}/domi.js' ily.dev/domi
+//	go list -f '{{.Dir}}/domi.js' ily.dev/domi
 //
 // Include this path in the app's JavaScript bundle.
 // Then import the module, and call run:
